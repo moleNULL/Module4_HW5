3b73c38efe2f58d2af12da7b2624fef278f8ea1f
moleNULL <94998782+moleNULL@users.noreply.github.com>
2023-02-05 15:25:15 +0200

    Add StyleCop with settings.ruleset

--- EFCore_LazyLoading_LINQ/Program.cs
+++ EFCore_LazyLoading_LINQ/Program.cs
@@ -1,10 +1,10 @@
 ﻿namespace EFCore_LazyLoading_LINQ
 {
-    internal class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("");
+            Console.WriteLine(" ");
         }
     }
 }
